248c48dc361ccddd2147d61163195b7ac6f13db9
woo94 <bestman21c@gmail.com>
2022-10-02 02:32:49 +0900

    Compose the Detail View

--- building-landmark-app/ContentView.swift
+++ building-landmark-app/ContentView.swift
@@ -18,6 +18,31 @@ struct ContentView: View {
      */
     var body: some View {
         /**
+         VStack을 하나 추가하고 custom MapView를 VStack의 제일 상단에 추가한다.
+         */
+        VStack {
+            MapView()
+            /**
+             Map content를 스크린의 top edge까지 확장시키기 위해서 ignoresSafeArea(edge: .top) modifier를 추가한다.
+             */
+                .ignoresSafeArea(edges: .top)
+            /**
+             MapView의 크기를 frame(width:height:)를 사용하여 설정한다.
+             height parameter만 설정하면 view는 자동으로 content의 width로 크기를 설정한다. 이번 경우에는 MapView는 가능한 공간을 모두 채운다.
+             */
+                .frame(height: 300)
+            
+            /**
+             CircleImage view를 stack에 추가한다.
+             */
+            CircleImage()
+            /**
+             Image를 map view의 위에 올리기 위해서 offset을 수직으로 -130을 주고, padding을 -130을 줬다.
+             */
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+        /**
          기본적으로 stack은 그들의 content를 축에 대해 가운데 정렬을 하고, context-appropriate spacing을 준다.
          
          VStack initializer를 수정하여 view들을 leading edges로 정렬시켜보자.
@@ -33,7 +58,6 @@ struct ContentView: View {
              */
             HStack {
                 Text("Joshua Tree National Park")
-                    .font(.subheadline)
                 /**
                  Layout보고 device의 full width를 사용하게 하기 위해 2개의 Text 사이에 Spacer를 추가하였다.
                  
@@ -41,10 +65,27 @@ struct ContentView: View {
                  */
                 Spacer()
                 Text("California")
-                    .font(.subheadline)
             }
+            /**
+             subheadline font modifier를 각각의 Text view에서 그들을 포함하고 있는 HStack으로 이동시킨다. 그리고 그들에게 secondary color를 적용한다.
+             
+             Modifier를 stack같은 layout view에 적용하면, SwiftUI는 포함하는 모든 요소에 modifier를 적용한다.
+             */
+            .font(.subheadline)
+            .foregroundColor(.secondary)
+            
+            Divider()
+            
+            Text("About Turtle Rock")
+                .font(.title2)
+            Text("Descriptive text goes here")
         }
         .padding()
+            /**
+             제일 바깥 VStack의 아래에 Spacer를 추가해주어서 content를 스크린의 상단으로 민다.
+             */
+            Spacer()
+        }
     }
 }
 
