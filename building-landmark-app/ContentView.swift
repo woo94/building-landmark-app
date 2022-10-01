2d361877cd40e0d74ba906af33b51bf38a8925ac
woo94 <bestman21c@gmail.com>
2022-10-02 01:15:32 +0900

    Create a New Project and Explore the Canvas

--- building-landmark-app/ContentView.swift
+++ building-landmark-app/ContentView.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+/**
+ 기본적으로 SwiftUI view는 2개의 structure를 선언한다.
+ 첫번째 structure는 View protocol을 따르고 view의 content와 layout을 나타낸다.
+ 두번째 structure는 그 view에 대한 preview를 선언한다.
+ */
 struct ContentView: View {
     var body: some View {
         VStack {
--- building-landmark-app/building_landmark_appApp.swift
+++ building-landmark-app/building_landmark_appApp.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+/**
+ SwiftUI app life cycle을 사용하는 app은 App protocol을 따른다.
+ Structure의 body property는 하나 혹은 그보다 많은 scene을 return한다.
+ @main attribute는 app의 entry point를 나타내준다.
+ */
 @main
 struct building_landmark_appApp: App {
     var body: some Scene {
