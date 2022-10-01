aae356c99276bd50b6a2985dcec1c00351f1a611
woo94 <bestman21c@gmail.com>
2022-10-02 01:04:28 +0900

    Initial Commit

---
+++ building-landmark-app/building_landmark_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  building_landmark_appApp.swift
+//  building-landmark-app
+//
+//  Created by woo94 on 2022/10/02.
+//
+
+import SwiftUI
+
+@main
+struct building_landmark_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
